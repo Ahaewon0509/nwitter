73ddf2d756714ad451605d622688517409df425b
Ahaewon0509 <https://github.com/Ahaewon0509/nwitter.git>
2022-05-04 20:22:31 +0900

    add code01

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Profile from "routes/Profile";
 import Navigation from "./Navigation";
 
 const AppRouter = ( {isLoggedIn} ) => {
@@ -11,9 +12,14 @@ const AppRouter = ( {isLoggedIn} ) => {
             {isLoggedIn && <Navigation />}
             <Switch>
         {isLoggedIn ? (
+            <>
             <Route exact path="/">
                 <Home />
             </Route>
+            <Route exact path="/profile">
+                <Profile />
+            </Route>
+            </>
         ) : (
             <Route exact path="/">
                 <Auth />
