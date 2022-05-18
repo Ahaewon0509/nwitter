27e94f8e574ee9550a3f6801be7af919b8b713d4
Ahaewon0509 <https://github.com/Ahaewon0509/nwitter.git>
2022-05-18 21:19:13 +0900

    code plse

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -4,9 +4,9 @@ const Nweet = ({ nweetObj, isOwner }) => {
     const onDeleteClick = async () => { //버튼에 삭제 기능 추가, 아래 button: "Delete Nweet"부분에 onClick={onDeleteClick} 추가
         const ok = window.confirm("삭제하시겠습니까?");
         console.log(ok);
-        if (ok) { //삭제를 <확인> 누르면, 트윗을 삭제하는 코드
+        if (ok) { //삭제를 <확인> 누르면, 트윗을 삭제하는 코드(삭제를 확인하면, if문에 true로 나타나고, 액션을 취함/ 취소를 하면 false값이 나타내고, 액션을 취하지 않고 바로 나오게 됨)
             console.log(nweetObj.id);
-            const data = await dbService.doc(`nweets/${nweetObj.id}`); // `표시는 숫자1 옆에있는 백틱
+            const data = await dbService.doc(`nweets/${nweetObj.id}`).delete(); // `표시는 숫자1 옆에있는 백틱
             console.log(data);
         }
     };
