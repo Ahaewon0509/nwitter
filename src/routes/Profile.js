cebc48a2a4636b33e8cd14a4fdaa161156468897
Ahaewon0509 <https://github.com/Ahaewon0509/nwitter.git>
2022-06-08 21:00:55 +0900

    code plus

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,21 @@
-import { authService } from "fbase";
+import { authService, dbService } from "fbase";
+import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
     const history = useHistory();
 
     const onLogOutClick = () => {
         authService.signOut();
         history.push("/");
     };
+
+    const getMyNweets = async () => {
+        const nweets = await dbService.collection("nweets")
+        .where("creatorId", "==", userObj.uid);
+    };
+
+    useEffect(() => {}, []);
 
     return (
         <>
